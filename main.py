899106aeb06b88b145677db4a88258e674892a58
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2021-12-04 23:29:17 +0100

    change path on images

--- main.py
+++ main.py
@@ -3,19 +3,45 @@ import os
 import time
 import random
 
+#Initialize a window or screen for display 
+WIDTH, HEIGHT = 750, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Invaders")  
+
 #Load images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_blue_small.png"))
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
 
 #Player ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_yellow_small.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 #Laser images
-RED_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_green.png"))
-BLUE_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_yellow.png"))
+RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 #Background
-BG = pygame.image.load(os.path.join("assets", "images", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT)
+
+#Function that runs game while window remains open
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+    
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+        
+        pygame.display.update()
+    
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+main()
