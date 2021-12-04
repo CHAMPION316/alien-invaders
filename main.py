36b759d92f546c48cdc24e620a2bfbefc0c3ee84
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2021-12-04 11:25:05 +0100

    add laser images and background

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import os
 import time
 import random
 
-#LOAD IMAGES
+#Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_blue_small.png"))
 
-#PLAYER SHIP
+#Player ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "images", "pixel_ship_yellow_small.png"))
+
+#Laser images
+RED_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_red.png"))
+GREEN_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_green.png"))
+BLUE_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_blue.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets", "images", "pixel_laser_yellow.png"))
+
+#Background
+BG = pygame.image.load(os.path.join("assets", "images", "background-black.png"))
