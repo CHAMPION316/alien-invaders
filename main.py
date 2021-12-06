18fafc9a61512cbddd7b774822cc02f2739224b0
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2021-12-06 02:39:18 +0100

    change lines 43/44 from blitz to blit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 #Initialize a window or screen for display 
 WIDTH, HEIGHT = 750, 750
@@ -26,13 +27,24 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 #Function that runs game while window remains open
+#Fucntion that determines player lives and levels
+
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 6
+    main_font = pygame.font.SysFont("cambriamath", 60)
     clock = pygame.time.Clock()
     
     def redraw_window():
         WIN.blit(BG, (0,0))
+        #Draw text
+        lives_label = main_font.render(f"Lives: {lives}", 1, (128, 255, 0))
+        level_label = main_font.render(f"Level: {level}", 1, (128, 255, 0))
+        
+        WIN.blit(lives_label, (20, 20))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 20, 20))
         
         pygame.display.update()
     
