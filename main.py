e8cf0b808a083122f534e9ea2b4168a7cb40e261
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2021-12-06 21:58:38 +0100

    add draw window, begin key input, add comments

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import time
 import random
 pygame.font.init()
 
-#Initialize a window or screen for display 
+#.............................Initialize a window or screen for display 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invaders")  
 
-#Load images
+#..............................Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
 
-#Player ship
+#.............................Player ship
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 #Laser images
@@ -23,11 +23,11 @@ GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
-#Background
+#...................................Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 class Ship: 
-    def __init__(self, x, y, color, health = 100):
+    def __init__(self, x, y, health = 100):
         self.x = x
         self.y = y
         self.health = health
@@ -35,10 +35,14 @@ class Ship:
         self.laser_image = None
         self.lasers = []
         self.cool_down_counter = 0
-        
+    
+#............................Function that draws size of ship on the window    
+    
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 255, 51), (self.x, self.y, 30, 30))
 
-#Function that runs game while window remains open
-#Fucntion that determines player lives and levels
+#............................Function that runs game while window remains open
+#............................Fucntion that determines player lives and levels
 
 def main():
     run = True
@@ -46,6 +50,10 @@ def main():
     level = 1
     lives = 6
     main_font = pygame.font.SysFont("cambriamath", 60)
+    
+#.............................calling the class ship, inputs for ships location on the window    
+    ship = Ship(350, 700)
+    
     clock = pygame.time.Clock()
     
     def redraw_window():
@@ -57,6 +65,9 @@ def main():
         WIN.blit(lives_label, (20, 20))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 20, 20))
         
+#...........................Draw window for the appearance of the ship     
+        ship.draw(WIN)
+        
         pygame.display.update()
     
     while run:
@@ -66,5 +77,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
+        
+        #..................................Key input for user movement
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]: #.........................Left
+            ship.x -= 1             
 main()
