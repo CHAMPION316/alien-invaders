a1749b4fc97c357ba67370f2f0fa6e7ecefd20b0
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2021-12-07 17:49:17 +0100

    change square icon to ship image and variable ship to player

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
 
-#.............................Player ship
+#.............................Player player
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 #Laser images
@@ -26,21 +26,31 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 #...................................Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+#....................................Class player's character and it's attributes
 class Ship: 
     def __init__(self, x, y, health = 100):
         self.x = x
         self.y = y
         self.health = health
-        self.ship_image = None
+        self.player_image = None
         self.laser_image = None
         self.lasers = []
         self.cool_down_counter = 0
-    
-#............................Function that draws size of ship on the window    
-    
+        
+    #............................Function that draws size of player on the window (temporary design *rectangle*)
     def draw(self, window):
-        pygame.draw.rect(window, (255, 255, 51), (self.x, self.y, 30, 30))
+        #pygame.draw.rect(window, (255, 255, 51), (self.x, self.y, 30, 30))
+        window.blit(self.ship_img, (self.x, self.y))
+        
 
+class Player(Ship):
+    def __init__(self, x, y, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SPACE_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+       
 #............................Function that runs game while window remains open
 #............................Fucntion that determines player lives and levels
 
@@ -53,8 +63,8 @@ def main():
     
     player_speed = 7
     
-#.............................calling the class ship, inputs for ships location on the window    
-    ship = Ship(350, 700)
+#.............................calling the class player, inputs for players location on the window    
+    player = Player(350, 700)
     
     clock = pygame.time.Clock()
     
@@ -67,8 +77,8 @@ def main():
         WIN.blit(lives_label, (20, 20))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 20, 20))
         
-#...........................Draw window for the appearance of the ship     
-        ship.draw(WIN)
+#...........................Draw window for the appearance of the player     
+        player.draw(WIN)
         
         pygame.display.update()
     
@@ -82,12 +92,12 @@ def main():
         
         #..................................Key input for user movement
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and ship.x - player_speed > 0: #.........................Left
-            ship.x -= player_speed
-        if keys[pygame.K_d] and ship.x + player_speed + 30 < WIDTH: #.........................Right
-            ship.x += player_speed
-        if keys[pygame.K_s] and ship.y + player_speed + 30 < HEIGHT: #.........................Down
-            ship.y += player_speed
-        if keys[pygame.K_w] and ship.y - player_speed > 0: #.........................Up
-            ship.y -= player_speed             
+        if keys[pygame.K_a] and player.x - player_speed > 0: #.........................Left
+            player.x -= player_speed
+        if keys[pygame.K_d] and player.x + player_speed + 30 < WIDTH: #.........................Right
+            player.x += player_speed
+        if keys[pygame.K_s] and player.y + player_speed + 30 < HEIGHT: #.........................Down
+            player.y += player_speed
+        if keys[pygame.K_w] and player.y - player_speed > 0: #.........................Up
+            player.y -= player_speed             
 main()
